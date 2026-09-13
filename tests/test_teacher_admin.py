ddbe351b045816413cff8290093234754ca16545
Your Name <your@email.com>
2026-09-13 11:26:57 +0300

    updated the test format to be simpler

--- tests/test_teacher_admin.py
+++ tests/test_teacher_admin.py
@@ -5,31 +5,20 @@ from models.teacher_admin import TeacherAdmin
 def run_tests():
     admin = TeacherAdmin("data/test_students.json")
 
-    print(
-    admin.add_student(
-        "S002",
-        "Brian",
-        "Software Engineering",
-        "B",
-        {}
-               )
-               )
-
-    print(admin.add_unit("S002", "Mathematics", 90))
-    print(admin.add_unit("S002", "English", 85))
-
-    print(admin.display_student("S002"))
-
+    admin.add_student( "S002","Brian","Software Engineering","B",{})
+    admin.add_unit("S002", "Mathematics", 90)
+    admin.add_unit("S002", "English", 85)
+    admin.display_student("S002")
       # Update Mathematics by adding it again with a new score.
-    print(admin.add_unit("S002", "Mathematics", 95))
-
-    print(admin.display_student("S002"))
-
-    print(admin.delete_student("S002"))
-
+    admin.add_unit("S002", "Mathematics", 95)
+    admin.display_student("S002")
+    admin.delete_student("S002")
       # This should now display: Student not found
-    print(admin.display_student("S002"))
+    admin.display_student("S002")
     
 if __name__=="__main__":
-   run_tests()
-   print("Test passed succesifuly")
+   try:
+       run_tests()
+       print("All tests passed")
+   except Exception as e:
+       print("Test failed:", e)
